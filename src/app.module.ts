44274137668abe44b47dee7b769d1a85053c08f0
nicolassanchez1 <nicolassanchez115@hotmail.com>
2026-03-24 15:53:06 -0500

    feat: remove extra SSL

--- src/app.module.ts
+++ src/app.module.ts
@@ -23,11 +23,6 @@ import { JwtStrategy } from './products/auth/jwt.strategy';
         autoLoadEntities: true,
         synchronize: true,
         ssl: process.env.DB_SSL === 'true' ? { rejectUnauthorized: false } : false,
-        extra: {
-          ssl: {
-            rejectUnauthorized: false,
-          },
-        },
       }),
     }),
     ProductsModule,
